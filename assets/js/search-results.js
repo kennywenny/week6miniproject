8afd2b252affdf29ed46d42f0a1ec231c9b01bba
Kenny Hung (he) <khung@instructors.2u.com>
2022-11-01 20:48:00 +0000

    Extracting the parameters from the second HTML page

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -1,3 +1,19 @@
+const queryVariables = window.location.search.substring(1)
+const querySplit = queryVariables.split('&')
+let format
+let searchTerm
+for (let i = 0; i < querySplit.length; i++) {
+  const splitAgain = querySplit[i].split('=')
+  if (splitAgain[0] === 'q') {
+    searchTerm = splitAgain[1]
+  }
+  if (splitAgain[0] === 'format') {
+    format = splitAgain[1]
+  }
+}
+console.log("Format", format)
+console.log("Search term", searchTerm)
+
 async function submitHandler(e) {
   e.preventDefault()
   const url = `https://www.loc.gov/${format}/?q=${searchTerm}&fo=json`
