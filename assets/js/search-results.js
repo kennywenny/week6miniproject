f5be98d3ed3a65d69ab656ba4d09afbc0b7d8c34
Kenny Hung (he) <khung@instructors.2u.com>
2022-11-01 21:00:31 +0000

    Loop through results and make some HTML for them

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -19,8 +19,24 @@ async function search(searchTerm, format) {
   const result = await fetch(url)
   const data = await result.json()
   console.log(data)
-  const featuredItems = data.featured_items
-  for (let i = 0; i < featuredItems.length; i++) {
-    console.log(featuredItems[i])
+  const results = data.results
+  for (let i = 0; i < results.length; i++) {
+    const result = results[i]
+    const listElement = document.createElement('ul')
+    Object.keys(result)
+      .map(it => {
+        const listItemElement = document.createElement('li')
+        listItemElement.textContent = `${it}: ${results[it]}`
+        listElement.appendChild(listItemElement)
+      })
+    document.querySelector('#cards').appendChild(listElement)
   }
 }
+
+/*
+  {
+      "image": "/static/collections/rochambeau-maps/images/ar110600.jpg",
+      "title": "Plan de New-York et des environs, levé par Montrésor, ingénieur en 1775.",
+      "url": "/item/gm71000988"
+    },
+  */
