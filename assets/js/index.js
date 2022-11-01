f94d4e83e6a16bc5ca1ba9295980ffe41587ed94
Kenny Hung (he) <khung@instructors.2u.com>
2022-11-01 20:27:44 +0000

    Submit handler on the button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,10 @@
-console.log('Hello world')
+const searchTextBox = document.querySelector("#search_term")
+const formatDropdown = document.querySelector("#format")
+const submitButton = document.querySelector("button")
+
+submitButton.addEventListener("click", submitHandler)
+
+function submitHandler(e) {
+  e.preventDefault()
+  console.log("You're submissive")
+}
